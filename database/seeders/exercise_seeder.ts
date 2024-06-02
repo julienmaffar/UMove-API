ed06de90a10f44f1c350039d0d31de49274bdb4b
Julien <julien.maffar@hotmail.com>
2024-06-02 16:39:13 +0200

    feat: ✨ add exercises seeder

--- database/seeders/exercise_seeder.ts
+++ database/seeders/exercise_seeder.ts
@@ -1,7 +1,25 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import { promises as fs } from 'node:fs'
+import { parse } from 'csv-parse/sync'
+import Exercise from '#models/exercise'
 
 export default class extends BaseSeeder {
+  static environment = ['development']
+
   async run() {
-    // Write your database queries inside the run method
+    const content = await fs.readFile(`./fakeData/exercices.csv`)
+    const records = parse(content, {
+      columns: true,
+      skip_empty_lines: true,
+    })
+
+    for (const record of records) {
+      await Exercise.create({
+        id: record.id,
+        name: record.name,
+        picture: record.picture,
+        animation: record.animation,
+      })
+    }
   }
 }
